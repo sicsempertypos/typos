4383c82684b348342acef2f7eba92e81127c317c
silverskunk <silverskunk@gmail.com>
2015-10-25 14:18:41 -0400

    starting php validation

--- js/contactform.js
+++ js/contactform.js
@@ -1,3 +1,20 @@
 <script>
-//	$(':text:first').focus;
+	<script src="js/jquery.min.js"></script>
+	<script src="js/"
+	<script src="js/bootstrap.min.js"></script>
+	<!--<script src="•http://cdn.jsdelivr.net/jquery.validation/1.14.0/jquery.validate.min.js"> </script>-->
+	<!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+	<script src="js/ie10-viewport-bug-workaround.js"></script>
+	<script src="js/preserveAliases.js"></script>
+	<script src="js/jquery.tablesorter.js"></script>
+	<script src="js/jquery.validate.min.js"></script>
+</script>
+<script>
+	function validate(contactform_fs) {
+		fail = validateName(contactform_fs.username.value)
+		fail += validateEmail(contactform_fs.email.value)
+		
+		if (fail == '') return true
+		else {alert(fail); return false}
+	}
 </script>

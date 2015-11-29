9e5682c930995d1b41db25a2fc3d1770b404cca9
silverskunk <silverskunk@gmail.com>
2015-11-28 21:16:05 -0500

    Fresh start

--- contact.php
+++ contact.php
@@ -1,5 +1,20 @@
 <?php
 	require_once 'login.php';
-	$conn = new mysqli($hn, $un, $pw, $db);
-	if ($conn->connect_error) die($conn->connect_error);
+	$link = new mysqli($hn, $un, $pw);
+
+	if (!$link) {
+    	die('failed to connect to server: ' . mysqli_connect_error());
+	}
+	if (!@mysqli_select_db($link, 'typos')) {
+		die('failed to connect to the database: ' . mysqli_error($link));
+	}
+	else {
+		printf ('it worked');
+	}
+	$query = "SELECT * FROM sicsemperinforequest";
+	$result = $link->query($query);
+	if (!$result) die ($conn->error);
+	echo $result;
+
+
 ?>

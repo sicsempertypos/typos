00c1dedc42432226365d7049b136e31b68e8f15f
sicsempertypos <silverskunk@gmail.com>
2016-11-12 23:42:19 -0500

    refining .gitignore

--- contact_index.php
+++ contact_index.php
@@ -2,58 +2,75 @@
 <html>
 <head>
 	<title>contact_index.php</title>
+	<link rel="stylesheet" type-="text/css" href="../css/normalize.css">
+	<link rel="stylesheet" type="text/css" href="../css/stylecontact.css">
 </head>
 <body>
-	<section>
-		<h2>Contact Sic Semper Typos</h2>
-		<form method="POST" action="user_contact.php" id="contactform" name="contactinput">
-			<fieldset>
-				<legend>Contact Information</legend>
-				<table>
-				<tr><td>First Name*</td><td><input type="text" id="fname" size="70"
-						class="forminput" autofocus required></td>
-					<td><span class="errmsg id="errfname"></span></td></tr>
-				<tr><td>Last Name*</td><td><input type="text" id="lname" size="70"
-						class="forminput" required></td>
-					<td><span id="errlname" class="errmsg"></span></td></tr>
-				<tr><td>Phone</td><td><input type="tel" id="phone" size="70"
-						class="forminput" optional></td>
-					<td><span id="errphone" class="errmsg"></span></td></tr>
-				<tr><td>Email*</td><td><input type="email" id="email" size="70"
-						class="forminput" required></td>
-					<td></td><span id="erremail" class="errmsg"></span></td></tr>
-				</table>
-			</fieldset>
-			<fieldset>
-				<legend>Sample Edit</legend>
-				<label for="request">Please edit my sample - 1000 characters max.</label>
-					<input type="checkbox" id="request" class="forminput">
-					<br>
-				<textarea id="sample" class="forminput" maxlength="1000" wrap="soft"
-					cols="70" rows="6" ></textarea>
-			</fieldset>
-			<fieldset>
-				<Legend>Service Interests</Legend>
-				<input type="checkbox" id="proof" 
-						class="forminput"><label for="proof">Proofreading</label>
-				<input type="checkbox" id="copy" 
-						class="forminput"><label for="copy">Copy Editing</label>
-				<input type="checkbox" id="publish" 
-						class="forminput"><label for="publish">ePublish</label>
-				<input type="checkbox" id="format" 
-						class="forminput"><label for="format">Formatting</label>
-				<input type="checkbox" id="other" 
-						class="forminput"><label for="other">Other (Explain)</label>
-			</fieldset>
-			<fieldset>
-				<legend>Comments</legend>
-				<textarea id="comments" class="forminput" cols="70" rows="4" value=""></textarea>
-			</fieldset>
-				<p><input type="submit" value="Submit"><input type="reset" value="Clear Form">
-					<input type="button" value="email John">
-				</p>
-		</form>
-
+	<section class="contact-form">
+		<!--<img src="../img/scroll/anniv-old-paper.png">-->
+		<div class="form-box">
+			<div class="wrapper">
+				<h2>Contact Sic Semper Typos</h2>
+				<form method="POST" action="contact_process.php">
+					<fieldset>
+						<legend>Contact Information</legend>
+						First Name* <span class="lineup"><input type="text" name="fname" id="fname" 
+								class="forminput" required></span>
+							<br><br>
+							<span id="errfname" class="errmsg"></span>
+							<wbr>
+						Last Name* <span class="lineup"><input type="text" name="lname" id="lname" 
+								class="forminput" required></span>
+							<br><br>
+							<span id="errlname" class="errmsg"></span>
+							<wbr>
+						Phone <span class="lineup"><input type="tel" name="phone" id="phone"
+								class="forminput" optional></span>
+							<br><br>
+							<span id="errphone" class="errmsg"></span>
+							<wbr>
+						Email* <span class="lineup"><input type="email" name="email" id="email"
+								class="forminput" required></span>
+							<br<br>>
+							<span id="erremail" class="errmsg"></span>
+							<wbr>
+					</fieldset>
+					<fieldset>
+						<legend>Sample Edit</legend>
+						<br>
+							<input type="checkbox" name="request" id="request" class="forminput">
+								Please edit my sample - 1000 characters max.
+							<br>
+						<textarea name="sample" id="sample" class="forminput" maxlength="1000" wrap="soft"
+							cols="70" rows="6" ></textarea>
+					</fieldset>
+					<fieldset>
+						<Legend>Service Interests</Legend>
+						<input type="checkbox" name="proof" id="proof" 
+								class="forminput"><label for="proof">Proofreading</label>
+						<br>
+						<input type="checkbox" name="copy" id="copy" 
+								class="forminput"><label for="copy">Copy Editing</label>
+						<br>
+						<input type="checkbox" name="publish" id="publish" 
+								class="forminput"><label for="publish">ePublish</label>
+						<br>
+						<input type="checkbox" name="format" id="format" 
+								class="forminput"><label for="format">Formatting</label>
+						<br>
+						<input type="checkbox" name="other" id="other" 
+								class="forminput"><label for="other">Other (Explain)</label>
+					</fieldset>
+					<fieldset>
+						<legend>Comments</legend>
+						<textarea name="comments" id="comments" class="forminput" cols="70" rows="4" value=""></textarea>
+					</fieldset>
+						<p><input type="submit" name="submit" value="Submit"><input type="reset" name="reset" value="Clear Form">
+							<input type="button" name="john" value="email John">
+						</p>
+				</form>
+			</div>
+		</div>
 	</section>
 </body>
 </html>
--- php/contact_index.php
+++ php/contact_index.php
@@ -12,33 +12,38 @@
 			<div class="wrapper">
 				<h2>Contact Sic Semper Typos</h2>
 				<form method="POST" action="contact_process.php">
-					<fieldset>
+					<fieldset class="container">
 						<legend>Contact Information</legend>
-						First Name* <span class="lineup"><input type="text" name="fname" id="fname" 
-								class="forminput" required></span>
-							<br><br>
-							<span id="errfname" class="errmsg"></span>
-							<wbr>
-						Last Name* <span class="lineup"><input type="text" name="lname" id="lname" 
-								class="forminput" required></span>
-							<br><br>
-							<span id="errlname" class="errmsg"></span>
-							<wbr>
-						Phone <span class="lineup"><input type="tel" name="phone" id="phone"
-								class="forminput" optional></span>
-							<br><br>
-							<span id="errphone" class="errmsg"></span>
-							<wbr>
-						Email* <span class="lineup"><input type="email" name="email" id="email"
-								class="forminput" required></span>
-							<br<br>>
-							<span id="erremail" class="errmsg"></span>
-							<wbr>
+						<div class="flex-wrapper">
+							<div class="box-contact">
+								<div class="col-1">
+									First Name * <br>
+									Last Name * <br>
+									Phone <br>
+									Email * <br>
+								</div>
+								<div class="col-2">	
+									<input type="text" name="fname" id="fname" 
+										class="forminput lineup" required>
+									<input type="text" name="lname" id="lname" 
+										class="forminput lineup" required>
+									<input type="tel" name="phone" id="phone"
+										class="forminput lineup" optional>
+									<input type="email" name="email" id="email"
+										class="forminput lineup" required	
+								</div>
+								<div class="col=3">
+									<div id="errfname" class="errmsg">	</div>
+									<div id="errlname" class="errmsg">	</div>
+									<div id="errphone" class="errms"> 	</div>
+									<div id="erremail" class="errmsg">	</div>
+								</div>
+							</div>
+						</div>
 					</fieldset>
 					<fieldset>
 						<legend>Sample Edit</legend>
-						<br>
-							<input type="checkbox" name="request" id="request" class="forminput">
+							<input type="checkbox" name="request" id="request" class="forminputck">
 								Please edit my sample - 1000 characters max.
 							<br>
 						<textarea name="sample" id="sample" class="forminput" maxlength="1000" wrap="soft"
@@ -47,27 +52,29 @@
 					<fieldset>
 						<Legend>Service Interests</Legend>
 						<input type="checkbox" name="proof" id="proof" 
-								class="forminput"><label for="proof">Proofreading</label>
+								class="forminputck"><label for="proof">Proofreading</label>
 						<br>
 						<input type="checkbox" name="copy" id="copy" 
-								class="forminput"><label for="copy">Copy Editing</label>
+								class="forminputck"><label for="copy">Copy Editing</label>
 						<br>
 						<input type="checkbox" name="publish" id="publish" 
-								class="forminput"><label for="publish">ePublish</label>
+								class="forminputck"><label for="publish">ePublish</label>
 						<br>
 						<input type="checkbox" name="format" id="format" 
-								class="forminput"><label for="format">Formatting</label>
+								class="forminputck"><label for="format">Formatting</label>
 						<br>
 						<input type="checkbox" name="other" id="other" 
-								class="forminput"><label for="other">Other (Explain)</label>
+								class="forminputck"><label for="other">Other (Explain)</label>
 					</fieldset>
 					<fieldset>
 						<legend>Comments</legend>
 						<textarea name="comments" id="comments" class="forminput" cols="70" rows="4" value=""></textarea>
 					</fieldset>
-						<p><input type="submit" name="submit" value="Submit"><input type="reset" name="reset" value="Clear Form">
-							<input type="button" name="john" value="email John">
-						</p>
+					<fieldset class="buttons">
+						<input type="submit" class="button" name="submit" value="Submit">
+						<input type="reset" class="button" name="reset" value="Clear Form">
+						<input type="button" class="button" name="john" value="Email John">
+					</fieldset>
 				</form>
 			</div>
 		</div>
